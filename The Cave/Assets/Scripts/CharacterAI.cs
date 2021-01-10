e83ab35afa74180b5293de07cc56d7308b133ce2
Alistair-Hudson <70953755+Alistair-Hudson@users.noreply.github.com>
2021-01-10 12:19:20 +0200

    Created mouse control for moving a single character

---
+++ The Cave/Assets/Scripts/CharacterAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAI : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    Vector3 goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        goal = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                goal = hit.point;
+            }
+            print(goal);
+        }
+        navMeshAgent.SetDestination(goal);
+    }
+
+}
